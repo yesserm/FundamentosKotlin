2d37f99ac2ffe48cc83838117aaa80948fc8dfa9
yessermiranda13 <yesser.miranda13@est.unanleon.edu.ni>
2020-11-04 22:55:45 -0600

    Trabajando con Caracteres y cadenas de texto

---
+++ 4.CaracteresCadenasDeTexto/CaracteresCadenas.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+ 
+    var caracter:Char = 'a'
+    val valorA:Int = caracter.toInt()
+ 
+    println(caracter + " es " + valorA + " en ASCII")
+ 
+    var cadena:String = "Yesser"
+    
+    println(cadena)
+  
+}
