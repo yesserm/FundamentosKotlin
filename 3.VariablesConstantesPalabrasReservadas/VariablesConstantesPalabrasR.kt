e85cea6540ff0dcb699a5bb355384b94e3a42952
yessermiranda13 <yesser.miranda13@est.unanleon.edu.ni>
2020-11-04 22:50:30 -0600

    Agregando variables y constantes

---
+++ 3.VariablesConstantesPalabrasReservadas/VariablesConstantesPalabrasR.kt
@@ -0,0 +1,18 @@
+/*
+Programa de variables y contantes */
+ 
+fun main(args: Array<String>) {
+    
+    //usando variables
+    var edadFirulais = 18
+    edadFirulais = 15
+    var nombreFirulais = "Pinky Firulais"
+    nombreFirulais = "Juan"
+    println(nombreFirulais)
+
+    //usando constantes
+    val pi = 3.14
+    //pi=4 //no se puede por ser inmutable
+    println(pi*2)
+
+}
