1c46b9d2e096fc816686f23c96cf05508b7ae151
yessermiranda13 <yesser.miranda13@est.unanleon.edu.ni>
2020-11-09 14:44:59 -0600

    Uso del bucle While y Do While

---
+++ 11.WhileDoWhile/WhileDoWhile.kt
@@ -0,0 +1,15 @@
+/*Ciclos while do-while */
+fun main() {
+    var x = 0
+    //uso del bucle while   
+    while (x<=10){ 
+       if(x == 8) println(x)
+       x = x + 2
+    }
+
+    //uso del bucle do while
+    var y = 1   
+    do{  
+      print("Estoy adentro del do while")      
+    }while(y!=1)     
+}
